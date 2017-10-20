a97fe8d02cc339135a7f44b58d1c1f3de82fba9b
vishnu rao <vishnuhr@singtel.com>
2017-10-20 09:50:24 +0800

    Fix logout (#15)
    * fixing unable to logout
    
    * fixing broken code when oauth is not in use.

--- superset/connectors/druid/wso2pydruid.py
+++ superset/connectors/druid/wso2pydruid.py
@@ -22,10 +22,11 @@ class Wso2PyDruid(PyDruid):
 
     def _prepare_url_headers_and_body(self, query):
         headers, querystr, url = super(Wso2PyDruid, self)._prepare_url_headers_and_body(query)
-        oauth_access_token = session.get('oauth') #oauth_access_token is of type tuple
-        oauth_access_token_string = "".join(oauth_access_token) #convert tuple to string by join.
-        if utils.isNotEmpty(oauth_access_token_string):
-            wso2_access_bearer_token_value = "".join(["Bearer ", oauth_access_token_string])
-            headers['Authorization'] = wso2_access_bearer_token_value
+        if 'oauth' in session.keys():
+            oauth_access_token = session.get('oauth') #oauth_access_token is of type tuple
+            oauth_access_token_string = "".join(oauth_access_token) #convert tuple to string by join.
+            if utils.isNotEmpty(oauth_access_token_string):
+                wso2_access_bearer_token_value = "".join(["Bearer ", oauth_access_token_string])
+                headers['Authorization'] = wso2_access_bearer_token_value
         log.debug(headers)
         return headers, querystr, url
